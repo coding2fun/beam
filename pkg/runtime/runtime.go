ae6a9489f0ed0d8523f0517be10dfbff31c098a7
khekrn <kishorenayark@gmail.com>
2022-05-01 09:38:51 +0530

    Seperating the entities out of beam. The service which use beam have to implement the beam entity interface

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -4,7 +4,7 @@ import "coding2fun.in/beam/pkg/domain"
 
 // BeamRuntime beam contract for workflow integration
 type BeamRuntime interface {
-	StartWorkflow(workflowName, resourceId string, variables map[string]any) domain.WorkflowResponse
+	StartWorkflow(workflowId, workflowName string, variables map[string]any) domain.WorkflowResponse
 
 	ResumeWorkflow(workflowName string, variables map[string]any) domain.WorkflowResponse
 }
