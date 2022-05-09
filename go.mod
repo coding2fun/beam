fac1d5914e31684ba4e965d75bb40d224c1782d8
khekrn <kishorenayark@gmail.com>
2022-05-09 07:51:14 +0530

    Initial Commit for Parser

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module coding2fun.in/beam
 
 go 1.18
+
+require (
+	github.com/rs/zerolog v1.0.0
+	github.com/tidwall/gjson v1.14.1
+)
+
+require (
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+)
--- internal/core/parser/parser.go
+++ internal/core/parser/parser.go
@@ -1,1 +1,38 @@
 package parser
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/rs/zerolog/log"
+	"github.com/tidwall/gjson"
+)
+
+const json = `{"name":{"first":"Janet","last":"Prichard"},"age":47}`
+
+// Parser contract, Generates [WorkflowGraph] from workflow definition
+type Parser interface {
+	CreateWorkflowGraph(file *os.File) *WorkflowGraph
+}
+
+type jsonParser struct {
+}
+
+func NewJsonParser() Parser {
+	return &jsonParser{}
+}
+
+func (jp *jsonParser) CreateWorkflowGraph(file *os.File) *WorkflowGraph {
+	log.Info().Msg("in jsonParser: CreateWorkflowGraph")
+
+	workflowDefinition, err := ioutil.ReadAll(file)
+	if err != nil {
+
+	}
+
+	jsonContent := string(workflowDefinition)
+
+	value := gjson.Get(json, "name.last")
+	println(value.String())
+	return nil
+}
